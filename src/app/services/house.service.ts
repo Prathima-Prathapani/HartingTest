b8ee93c3591f5900493b54c25dedaa3b47895743
prathima <prathima.prathapani17@gmail.com>
2025-06-25 07:12:26 +0530

    add house service for two api calls
    1.getHouseData- fetches all house data as per given pagination
    
    2.getHouseById- fetches individual house data based on given id

---
+++ src/app/services/house.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Observable, map } from 'rxjs';
+
+export interface House {
+  url: string;
+  name: string;
+  region: string;
+  words: string;
+  titles: string[];
+  seats: string[];
+  founded: string;
+  overlord: string;
+  currentLord: string;
+}
+
+@Injectable({ providedIn: 'root' })
+export class HouseService {
+  private readonly api = 'https://anapioficeandfire.com/api/houses';
+
+  constructor(private http: HttpClient) {}
+
+ 
+  getHouseData(page = 1, pageSize = 20): Observable<House[]> {
+    const params = new HttpParams()
+      .set('page', page)
+      .set('pageSize', pageSize);
+    return this.http.get<House[]>(this.api, { params });
+  }
+   getHouseById(id: string): Observable<any> {
+    return this.http.get<any>(`${this.api}/${id}`);
+  }
+
+}
